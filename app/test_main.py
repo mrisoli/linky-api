a45bc3837d785385c87f6193237c749badb3c51d
Marcelo Risoli <celorisoli@gmail.com>
2022-03-10 17:21:38 -0600

    add test workflow action

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,16 @@
+"""
+tests for main app
+"""
 from fastapi.testclient import TestClient
 
-from main import app
+from .main import app
 
 client = TestClient(app)
 
 def test_read_main():
+    """
+    test GET /
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
